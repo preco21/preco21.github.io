85e1f7a3ccfa6459ec3ed2b09bbf388be5077670
Plusb Preco <plusb21@gmail.com>
2017-06-19 16:00:05 +0900

    Re-enable prepack plugin

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -8,7 +8,7 @@ import {
 import CleanPlugin from 'clean-webpack-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
 import HTMLPlugin from 'html-webpack-plugin';
-// import PrepackPlugin from 'prepack-webpack-plugin';
+import PrepackPlugin from 'prepack-webpack-plugin';
 import BabiliPlugin from 'babili-webpack-plugin';
 import OptimizeJSPlugin from 'optimize-js-plugin';
 import ExtractTextPlugin, {extract} from 'extract-text-webpack-plugin';
@@ -104,7 +104,7 @@ function config({dev} = {}) {
         new NamedModulesPlugin(),
         new NoEmitOnErrorsPlugin(),
       ] : [
-        // new PrepackPlugin(),
+        new PrepackPlugin(),
         new BabiliPlugin(),
         new OptimizeJSPlugin(),
         new ExtractTextPlugin({
