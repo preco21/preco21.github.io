d0e227ba63c5c730002e5260aed146ee60321ce0
Plusb Preco <plusb21@gmail.com>
2018-01-09 17:06:58 +0900

    Use good naming for css loader

--- webpack.config.js
+++ webpack.config.js
@@ -67,7 +67,7 @@ module.exports = ({dev, devServer, report} = {}) => {
                 options: {
                   sourceMap: true,
                   modules: true,
-                  localIdentName: '[name]__[local]___[hash:base64:5]',
+                  localIdentName: '[path][name]__[local]--[hash:base64:5]',
                 },
               },
             ],
